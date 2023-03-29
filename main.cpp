d26de7736cb051e6792b5a14fa9ff216f28b3f15
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-03-29 21:07:52 +0900

    opencv adjusting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <numeric>
+#include <algorithm>
+
 #include <opencv2/opencv.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    cv::Mat A(10, 10, CV_16U);
+    std::iota(A.begin<unsigned short>(), A.end<unsigned short>(), 0u);
+
+    for (decltype(A.rows) i = 0; i < A.rows; ++i)
+    {
+        auto pt = A.ptr<unsigned short>(i);
+        for (decltype(A.cols) j = 0; j < A.cols; ++j)
+        {
+            std::cout << pt[j] << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << "press any key";
+    char ch;
+    std::cin >> ch;
 }
