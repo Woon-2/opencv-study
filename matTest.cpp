d2f20f4cffcf9960093a7c8263a50140b58804e6
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-04-27 20:07:40 +0900

    testing opencv feature via gtest

--- matTest.cpp
+++ matTest.cpp
@@ -2,20 +2,33 @@
 #define __matTest
 
 #include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+
 #include <iostream>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <ranges>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(MatTest, assign)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
-}
+    auto A = cv::Mat(10, 10, CV_16U);
+    std::iota(A.begin<unsigned short>(), A.end<unsigned short>(),
+        unsigned short(0));
+    
+    auto V = std::vector<unsigned short>();
+    std::adjacent_difference(A.begin<unsigned short>(), A.end<unsigned short>(),
+        std::back_inserter(V));
 
-TEST(HelloTest, BasicAssertions2)
-{
-    EXPECT_EQ(1 + 2 + 3 + 4 + 5, 15);
+    std::ranges::for_each(V, [](const auto& diff) {
+        std::cout << diff << ' ';
+    });
+
+    GTEST_ASSERT_EQ(unsigned short(0), A.at<unsigned short>(0, 0));
+    std::ranges::for_each(V.begin() + 1, V.end(), [](const auto& diff) {
+        GTEST_ASSERT_EQ(1, diff);
+    });
 }
 
 TEST(Terminate, Terminate)
