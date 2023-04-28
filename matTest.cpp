8e09f54551f008eefa68d7038e7cb65c9fb965d8
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-04-28 12:44:42 +0900

    enhanced readibility with adding white space, renaming tests, and added a test copying temporary matrix which is result of matrix operation

--- matTest.cpp
+++ matTest.cpp
@@ -11,43 +11,55 @@
 #include <algorithm>
 #include <ranges>
 
-TEST(MatTest, GenerateWithCPPStandardAlgorithm)
+TEST(MatTest, GenerationWithCPPStandardAlgorithmWorks)
 {
     auto A = cv::Mat(10, 10, CV_16U);
-    std::iota(A.begin<unsigned short>(), A.end<unsigned short>(),
-        unsigned short(0));
+    std::iota( A.begin<unsigned short>(), A.end<unsigned short>(),
+        unsigned short(0) );
     
     auto V = std::vector<unsigned short>();
-    std::adjacent_difference(A.begin<unsigned short>(), A.end<unsigned short>(),
-        std::back_inserter(V));
+    std::adjacent_difference( A.begin<unsigned short>(), A.end<unsigned short>(),
+        std::back_inserter(V) );
     
-    EXPECT_EQ(unsigned short(0), A.at<unsigned short>(0, 0));
-    std::ranges::for_each(V.begin() + 1, V.end(), [](const auto& diff) {
+    EXPECT_EQ( unsigned short(0), A.at<unsigned short>(0, 0) );
+    std::ranges::for_each( V.begin() + 1, V.end(), [](const auto& diff) {
         EXPECT_EQ(1, diff);
-    });
+    } );
 }
 
-TEST(MatTest, CopyARowSlight)
+TEST(MatTest, CopyingLValueIsSlightCopy)
 {
     auto A = cv::Mat(200, 200, CV_32FC3);
-    cv::randu(A, cv::Scalar::zeros(), cv::Scalar::all(255));
+    cv::randu( A, cv::Scalar::zeros(), cv::Scalar::all(255.) );
 
     auto B = A.row(3);
 
-    A.row(3) += cv::Scalar::all( 1. );
+    B += cv::Scalar::all(1.);
 
     // the way to check two matrices are identical
     EXPECT_EQ( cv::sum(A.row(3) != B), cv::Scalar::zeros() );
 }
 
-TEST(MatTest, CopyARowDeep)
+TEST(MatTest, CopyingCloneIsDeepCopy)
 {
     auto A = cv::Mat(200, 200, CV_16UC4);
-    cv::randu(A, cv::Scalar::zeros(), cv::Scalar::all(255));
+    cv::randu(A, cv::Scalar::zeros(), cv::Scalar::all(255.));
 
     auto B = A.row(3).clone();
 
-    A.row(3) += cv::Scalar::all( 1. );
+    B += cv::Scalar::all(1.);
+
+    EXPECT_EQ( cv::sum(A.row(3) == B), cv::Scalar::zeros() );
+}
+
+TEST(MatTest, CopyingRvalueIsDeepCopy)
+{
+    auto A = cv::Mat(200, 200, CV_32FC3);
+    cv::randu(A, cv::Scalar::zeros(), cv::Scalar::all(255.));
+
+    auto B = A.row(3) + cv::Scalar::all(0.);
+
+    B += cv::Scalar::all(1.);
 
     EXPECT_EQ( cv::sum(A.row(3) == B), cv::Scalar::zeros() );
 }
